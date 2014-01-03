eb2dbe74f5272b646d6e984527e8ce27902ebac4
2010jr <jun.kusaha25@gmail.com>
2014-01-03 09:45:53 +0900

    Initial Commit

---
+++ PracticeCoding/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PracticeCoding
+//
+//  Created by 日鼻 旬 on 2014/01/03.
+//  Copyright (c) 2014年 Jun Kusahana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
